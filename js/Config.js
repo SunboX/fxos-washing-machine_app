dca7a531037c32d0aded3b7f0400193f7bdf5e3e
André Fiedler <andre.fiedler@me.com>
2015-03-30 22:47:28 +0200

    wip

--- js/Config.js
+++ js/Config.js
@@ -1,5 +1,5 @@
 wm.Config = {
-    washingMachineApi: 'http://192.168.1.139:8080',
+    washingMachineApi: 'http://192.168.137.149:8080',
     waterApiUrl: 'http://water-opendatade.rhcloud.com/water.json',
     washingPowderApiUrl: 'http://detergent-opendatade.rhcloud.com/washingPowder.json',
     fabricConditionerApiUrl: 'http://detergent-opendatade.rhcloud.com/fabricConditioner.json'
